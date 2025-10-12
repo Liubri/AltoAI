bd4878b650d8362af9695a8a13df5f39f749665b
Liubri <85087010+Liubri@users.noreply.github.com>
2025-10-11 20:23:06 -0400

    Added hugging face ai model and separated ai function

---
+++ alto-ai-server/ai.js
@@ -0,0 +1,39 @@
+import OpenAI from "openai"
+import dotenv from "dotenv"
+
+dotenv.config()
+const client = new OpenAI({
+	baseURL: "https://router.huggingface.co/v1",
+	apiKey: process.env.HF_TOKEN,
+});
+
+export async function getAIRequest(req, res) {
+  const prompt = req.query.prompt
+  try {
+    const playlist = await generatePlaylist(prompt);
+    res.json({ playlist });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+async function generatePlaylist(prompt) {
+  const response = await client.chat.completions.create({
+    model: "openai/gpt-oss-20b:nebius",
+    messages: [
+      { role: "system", content: "You are a music recommendation assistant." },
+      { role: "user", content: `Generate a playlist of 10 songs for: ${prompt}
+      Return **ONLY** a JSON array in this exact format:
+      [
+     { "title": "Song Name", "artist": "Artist Name" }
+    ]
+    Do not include any explanations or text.` }
+    ],
+    temperature: 0.7
+  });
+  const playlistJSON = response.choices[0].message.content;
+  console.log(JSON.stringify(response.choices[0].message));
+  console.log(response);
+  return JSON.parse(playlistJSON);
+}
--- alto-ai-server/server.js
+++ alto-ai-server/server.js
@@ -1,6 +1,9 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
+import { getAIRequest } from './ai.js'
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express();
 app.use(cors()); // allow frontend requests
 app.use(express.json());
@@ -8,6 +11,8 @@ app.use(express.json());
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node.js!' });
 });
+
+app.get('/api/openai', getAIRequest);
 
 const PORT = 3000;
 app.listen(PORT, () => {
